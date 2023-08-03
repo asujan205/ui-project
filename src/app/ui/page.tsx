5b77e99cc6068ce18139e29a709f0b95428328a8
asujan205 <asujan205@gmail.com>
2023-08-03 19:45:28 +0545

    #Changes

--- src/app/ui/page.tsx
+++ src/app/ui/page.tsx
@@ -25,7 +25,7 @@ export default function UiPage() {
               />
               <h1
                 className="text-[#FFF]
-font-playfairDisplay
+font-pairPlay
 text-[40px]
  normal
 font-[700] leading-normal"
@@ -33,16 +33,38 @@ font-[700] leading-normal"
                 <p>An Investment in Knowledge</p> pays the best interest
               </h1>
               <h1
-                className="text-[#FFF] font-playfairDisplay
+                className="text-[#FFF] font-pairPlay
 text-[24px]
  normal
-font-[700] leading-normal"
+font-[400] leading-normal"
               >
                 <p>It is a long established fact that a reader</p>
                 <p>will be distracted by the readable content</p>
                 of a page when.
               </h1>
+
+              <button className=" w-[180px] h-[76px] rounded-[20px] border-[#FFF] bg-[#FFF]  mt-[61px]">
+                <p
+                  className="text-[#CD1509] font-pairPlay
+text-[24px]
+  normal
+  font-[600] leading-normal"
+                >
+                  Learn More
+                </p>
+              </button>
             </div>
+          </div>
+          <div className="flex flex-col justify-between items-center h-full pt-[2.1rem]">
+            <div className="flex flex-row  justify-evenly text-black  ">
+              <div>
+                <h1>Home</h1>
+              </div>
+              <div>About</div>
+
+              <div>Services</div>
+            </div>
+            <img src="/group.png" className="pt-[50px]" />
           </div>
         </div>
 
